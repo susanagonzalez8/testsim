d34f7d9185139b87c674c7413b6f5940d88287df
Tomás Di Domenico <tdido@tdido.eu>
2022-10-27 15:53:18 +0200

    Improve readability in analyse_sample.sh

--- scripts/analyse_sample.sh
+++ scripts/analyse_sample.sh
@@ -1,24 +1,44 @@
-if [ "$#" -eq 1 ]
+if [ "$#" -ne 1 ]
 then
-    sampleid=$1
-    echo "Running FastQC..."
-    mkdir -p out/fastqc
-    fastqc -o out/fastqc data/${sampleid}*.fastq.gz
-    echo
-    echo "Running cutadapt..."
-    mkdir -p log/cutadapt
-    mkdir -p out/cutadapt
-    cutadapt -m 20 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT -o out/cutadapt/${sampleid}_1.trimmed.fastq.gz -p out/cutadapt/${sampleid}_2.trimmed.fastq.gz data/${sampleid}_1.fastq.gz data/${sampleid}_2.fastq.gz > log/cutadapt/${sampleid}.log
-    echo
-    echo "Running STAR index..."
-    mkdir -p res/genome/star_index
-    STAR --runThreadN 4 --runMode genomeGenerate --genomeDir res/genome/star_index/ --genomeFastaFiles res/genome/ecoli.fasta --genomeSAindexNbases 9
-    echo
-    echo "Running STAR alignment..."
-    mkdir -p out/star/${sampleid}
-    STAR --runThreadN 4 --genomeDir res/genome/star_index/ --readFilesIn out/cutadapt/${sampleid}_1.trimmed.fastq.gz out/cutadapt/${sampleid}_2.trimmed.fastq.gz --readFilesCommand zcat --outFileNamePrefix out/star/${sampleid}/
-    echo
-else
     echo "Usage: $0 <sampleid>"
     exit 1
 fi
+
+sampleid=$1
+
+echo "Running FastQC..."
+mkdir -p out/fastqc
+fastqc -o out/fastqc data/${sampleid}*.fastq.gz
+echo
+
+echo "Running cutadapt..."
+mkdir -p log/cutadapt
+mkdir -p out/cutadapt
+cutadapt \
+    -m 20 \
+    -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
+    -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
+    -o out/cutadapt/${sampleid}_1.trimmed.fastq.gz \
+    -p out/cutadapt/${sampleid}_2.trimmed.fastq.gz data/${sampleid}_1.fastq.gz data/${sampleid}_2.fastq.gz \
+    > log/cutadapt/${sampleid}.log
+echo
+
+echo "Running STAR index..."
+mkdir -p res/genome/star_index
+STAR \
+    --runThreadN 4 \
+    --runMode genomeGenerate \
+    --genomeDir res/genome/star_index/ \
+    --genomeFastaFiles res/genome/ecoli.fasta \
+    --genomeSAindexNbases 9
+echo
+
+echo "Running STAR alignment..."
+mkdir -p out/star/${sampleid}
+STAR \
+    --runThreadN 4 \
+    --genomeDir res/genome/star_index/ \
+    --readFilesIn out/cutadapt/${sampleid}_1.trimmed.fastq.gz out/cutadapt/${sampleid}_2.trimmed.fastq.gz \
+    --readFilesCommand zcat \
+    --outFileNamePrefix out/star/${sampleid}/
+echo
